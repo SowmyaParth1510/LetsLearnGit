abee07149934071a842375e7b59fd8931843bad3
Sowmya P <sowmyaparthasarathy15@gmail.com>
2024-08-12 01:45:37 +0530

    Initial Commit

---
+++ MyFirstPgm.java
@@ -0,0 +1,13 @@
+package TestLearn;
+
+public class MyFirstPgm {
+
+	public static void main(String[] args) {
+int a=8;
+int b=9;
+    System.out.println("Hello GIT!");
+    
+ System.out.println(a+b);
+	}
+
+}
